afbe1d1e3da49f6936c2dbbe0e9f0daf9084dbfa
Carlos Gomez [BT] <carlos.gomez@blautech.us>
2018-12-30 23:34:35 -0600

    Add Home tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HistoryComponent } from './components/history/history.component';
+import { SearchComponent } from './components/search/search.component';
+import { AccountComponent } from './components/account/account.component';
 
 
 @NgModule({
@@ -12,6 +16,10 @@ import { LoginComponent } from './components/login/login.component';
     AppComponent,
     HomeComponent,
     LoginComponent,
+    ProfileComponent,
+    HistoryComponent,
+    SearchComponent,
+    AccountComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,17 +9,85 @@ import { Page } from "tns-core-modules/ui/page";
 })
 export class HomeComponent implements OnInit {
   title = 'App Médica';
-  
+
   message = " Has iniciado exitosamente";
-  
+
+  lastDelY = 0;
+  headerCollapsed = false;
+  selectedTab = 0;
+  selectedTabview = 0;
+
   constructor(private page: Page, private routerExtensions: RouterExtensions) {
     this.page.actionBarHidden = true;
-    }
+  }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+  }
 
-    logout() {
-        this.routerExtensions.navigate(["/login"], { clearHistory: true });
+  logout() {
+    this.routerExtensions.navigate(["/login"], { clearHistory: true });
+  }
+
+  categoryIcon(itemCategory) {
+    switch (itemCategory) {
+      case "Burger":
+        return String.fromCharCode(0xf0f5); //"fa-cutlery";
+        break;
+      case "Beer":
+        return String.fromCharCode(0xf0fc); //"fa-beer";
+        break;
+      case "Pancake":
+        return String.fromCharCode(0xf0f4); //"fa-coffee";
+        break;
+      case "Cake":
+        return String.fromCharCode(0xf1fd); //"fa-birthday-cake";
+        break;
+      default:
+        return String.fromCharCode(0xf06d); //"fa-fire";
+        break;
     }
+  }
+
+  //Top nav bar tap methods
+  onSearchTap() {
+    this.selectedTab = 4;
+  }
+  
+  onBellTap() {
+    this.selectedTab = 5;
+  }
+
+  onAccount() {
+    this.selectedTab = 6;
+  }
+  
+
+  onPopularTap() {
+    this.selectedTabview = 0;
+  }
+
+  onCategoryTap() {
+    this.selectedTabview = 1;
+  }
+
+  onPromosTap() {
+    this.selectedTabview = 2;
+  }
+
+  //Bottom nav bar tap methods
+  onHomeTap() {
+    this.selectedTab = 0;
+  }
+
+  onCartTap() {
+    this.selectedTab = 1;
+  }
+
+  onHistoryTap() {
+    this.selectedTab = 2;
+  }
+
+  onAboutTap() {
+    this.selectedTab = 3;
+  }
 }
